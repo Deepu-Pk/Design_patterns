8f7c381f864fa2949d6871a0f4f88aee3f99e8cb
Deepu Pk <deepupk4007@gmail.com>
2025-03-16 15:06:15 +0530

    updated error_logger.py code

--- factory_method/simple/error_logger.py
+++ factory_method/simple/error_logger.py
@@ -3,7 +3,7 @@ from ilogger import ilogger
 
 class errorLogger(ilogger):
     def log(self, string : str):
-        print(f"[ERROR] {string}")
+        print(f"[ERROR] : {string}")
 
     def __del__(self) -> None:
         pass 
